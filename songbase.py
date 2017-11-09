07a5e60c07b0a057d6687541fc85165779bca9a7
Tiana Jarman <tjarman@udel.edu>
2017-11-09 13:47:31 -0500

    added flask framework

--- songbase.py
+++ songbase.py
@@ -1,16 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'hello world'
+    # return '<h1>hello world!!!!!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<string:name>')
 def get_user(name):
-    return '<h1>hello %s your age is %d</h1>' % (name, 3)
+    # return '<h1>hello %s your age is %d</h1>' % (name, 3)
+    return render_template('user.html', user_name=name)
+
+
+@app.route('/songs')
+def list_songs():
+    songs = [
+        'Supercut',
+        'Wolves',
+        'Holy Ground'
+    ]
+    return render_template('songs.html', songs=songs)
 
 
 if __name__ == '_main_':
-    app.run(debug=True)
+    app.run()
