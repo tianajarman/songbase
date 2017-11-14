b0136bcabdc704c13458316a3adbd69e1bdde903
Tiana Jarman <tjarman@udel.edu>
2017-11-14 13:41:52 -0500

    added web form

--- manage.py
+++ manage.py
@@ -3,5 +3,5 @@ from songbase import app
 
 manager = Manager(app)
 
-if __name__== '__main__':
+if __name__ == '__main__':
     manager.run()
--- songbase.py
+++ songbase.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, session, render_template, request, redirect, url_for
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'wersdfxcvt'
 
 
 @app.route('/')
@@ -24,5 +25,20 @@ def list_songs():
     return render_template('songs.html', songs=songs)
 
 
-if __name__ == '_main_':
+@app.route('/form-demo', methods=['GET', 'POST'])
+def form_demo():
+    if request.method == 'GET':
+        first_name = request.args.get('first_name')
+        if first_name:
+            return render_template('form-demo.html', first_name=first_name)
+        else:
+            first_name = session.get('first_name')
+            return render_template('form-demo.html', first_name=first_name)
+    if request.method == 'POST':  # could also use 'else' by itself
+        session['first_name'] = request.form['first_name']
+        # return render_template('form-demo.html', first_name=first_name)
+        return redirect(url_for('form_demo'))  # important endpoint after post
+
+
+if __name__ == '__main__':
     app.run()
