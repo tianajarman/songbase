97ecb463330850c7ebedfb12cb5b571da0eceec3
Tiana Jarman <tjarman@udel.edu>
2017-11-16 13:35:50 -0500

    added database tables

--- manage.py
+++ manage.py
@@ -1,7 +1,21 @@
 from flask_script import Manager
-from songbase import app
+from songbase import app, db, Artist, Song
 
 manager = Manager(app)
+
+
+@manager.command  # reset the database and create two artists
+def deploy():
+    db.drop_all()
+    db.create_all()
+    coldplay = Artist(name='Coldplay', about='Coldplay is a British rock band.')
+    maroon5 = Artist(name='Maroon 5', about='Maroon 5 is an American pop rock band.')
+    song1 = Song(name='yellow', year=2004, lyrics='blah blah', artist=coldplay)
+    db.session.add(coldplay)
+    db.session.add(maroon5)
+    db.session.add(song1)
+    db.session.commit()
+
 
 if __name__ == '__main__':
     manager.run()
